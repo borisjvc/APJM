9fcd31fd7048668329b66b94665e24b2c8854b64
borisjvc <boris_25@live.com.mx>
2023-11-11 20:14:56 -0500

    dashboard usuarios funcional

--- frontend/src/paginas/Dashboard/Dashboard_Usuarios.jsx
+++ frontend/src/paginas/Dashboard/Dashboard_Usuarios.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Button, Icon, Table, Modal, Input } from "semantic-ui-react";
+import { Button, Icon, Table, Modal, Input, Dropdown, Label } from "semantic-ui-react";
 import axios from "axios";
 
 export default function Usuarios() {
     const [usuarios, setUsuarios] = useState([]);
-    const [selectedUser, setSelectedUser] = useState(null);
+    const [newUser, setNewUser] = useState({
+        Username: "",
+        Email: "",
+        Passwrd: "",
+        rol: "Usuario",
+    });
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const [selectedUser, setSelectedUser] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [editedUser, setEditedUser] = useState({
         id: "",
         username: "",
         email: "",
-        rol: "",
+        passwrd: "",
+        rol: "Usuario",
     });
 
     useEffect(() => {
+        obtenerUsuarios();
+    }, []);
+
+    const obtenerUsuarios = () => {
         axios.get("http://localhost:3001/usuarios")
             .then((response) => {
                 const formattedUsers = response.data.map((user) => ({
@@ -28,53 +40,82 @@ export default function Usuarios() {
             .catch((error) => {
                 console.error("Error al obtener usuarios: ", error);
             });
-    }, []);
+    };
 
-    // Manejador para abrir el modal de edición
-    function handleEdit(user) {
-        setSelectedUser(user);
-        setEditedUser({
-            id: user.id,
-            username: user.username,
-            email: user.email,
-            rol: user.rol,
-        });
-        setIsModalOpen(true);
-    }
-
-    // Manejador para cerrar el modal
-    function handleCloseModal() {
-        setSelectedUser(null); // Limpia el usuario seleccionado
-        setIsModalOpen(false); // Cierra el modal
-    }
-
-    function handleSaveChanges() {
-        // Implementa la lógica para enviar los cambios a la base de datos
-        axios.put(`http://localhost:3001/usuarios/${editedUser.id}`, editedUser)
+    const handleEdit = (user) => {
+        axios.get(`http://localhost:3001/usuarios/${user.id}`)
             .then((response) => {
-                // Actualizar la lista de usuarios después de la edición
-                axios.get("http://localhost:3001/usuarios")
-                    .then((response) => {
-                        setUsuarios(response.data);
-                    })
-                    .catch((error) => {
-                        console.error("Error al obtener usuarios: ", error);
-                    });
+                const userData = response.data[0];
+                setSelectedUser(user);
+                setEditedUser({
+                    id: userData.ID,
+                    username: userData.Username,
+                    email: userData.Email,
+                    passwrd: userData.passwrd,
+                    rol: userData.Rol,
+                });
+                setIsModalOpen(true);
+            })
+            .catch((error) => {
+                console.error("Error al obtener datos del usuario: ", error);
+            });
+    };
 
+    const handleCloseModal = () => {
+        setSelectedUser(null);
+        setIsModalOpen(false);
+    };
+
+    const handleSaveChanges = () => {
+        axios.put(`http://localhost:3001/usuarios/${editedUser.id}`, {
+            Username: editedUser.username,
+            Passwrd: editedUser.passwrd,
+            Email: editedUser.email,
+            Rol: editedUser.rol,
+        })
+            .then((response) => {
+                obtenerUsuarios(); // Obtener la lista actualizada de usuarios
                 setIsModalOpen(false);
             })
             .catch((error) => {
                 console.error("Error al guardar los cambios: ", error);
             });
-    }
+    };
 
-    function handleDelete() {
-        //borrar usuario seleccionado de la tabla
-        console.log("usuario eliminado")
-    }
+    const handleDelete = async (id) => {
+        await axios.delete(`http://localhost:3001/usuarios/${id}`);
+        obtenerUsuarios();
+    };
+
+    const handleOpenAddModal = () => {
+        setIsAddModalOpen(true);
+    };
+
+    const handleCloseAddModal = () => {
+        setIsAddModalOpen(false);
+    };
+
+    const handleAddUser = () => {
+        axios.post("http://localhost:3001/usuarios", newUser)
+            .then((response) => {
+                obtenerUsuarios();
+                setIsAddModalOpen(false);
+                setNewUser({
+                    Username: "",
+                    Email: "",
+                    Passwrd: "",
+                    rol: "Usuario",
+                });
+            })
+            .catch((error) => {
+                console.error("Error al agregar el usuario: ", error);
+            });
+    };
+
 
     return (
         <>
+            <Button content="Agregar usuario" color="blue" icon='add' onClick={handleOpenAddModal}></Button>
             <Table celled inverted selectable>
                 <Table.Header>
                     <Table.Row>
@@ -95,12 +136,12 @@ export default function Usuarios() {
                             <Table.Cell>{user.email}</Table.Cell>
                             <Table.Cell>{user.rol}</Table.Cell>
                             <Table.Cell>
-                                <Button color="blue" icon onClick={handleEdit}>
+                                <Button color="blue" icon onClick={() => handleEdit(user)}>
                                     <Icon name="edit outline" />
                                 </Button>
                             </Table.Cell>
                             <Table.Cell>
-                                <Button color="red" icon onClick={handleDelete}>
+                                <Button color="red" icon onClick={() => handleDelete(user.id)}>
                                     <Icon name="trash alternate outline" />
                                 </Button>
                             </Table.Cell>
@@ -111,30 +152,75 @@ export default function Usuarios() {
 
             <Modal open={isModalOpen} onClose={handleCloseModal}>
                 <Modal.Header>Editar Usuario</Modal.Header>
+
                 <Modal.Content>
-                    {selectedUser && (
-                        <div>
-                            <Input
-                                label="Nombre de usuario"
-                                value={editedUser.username}
-                                onChange={(e) => setEditedUser({ ...editedUser, username: e.target.value })}
-                            />
-                            <Input
-                                label="Correo electrónico"
-                                value={editedUser.email}
-                                onChange={(e) => setEditedUser({ ...editedUser, email: e.target.value })}
-                            />
-                            <Input
-                                label="Rol"
-                                value={editedUser.rol}
-                                onChange={(e) => setEditedUser({ ...editedUser, rol: e.target.value })}
-                            />
-                        </div>
-                    )}
+                    <Input
+                        label="Nombre de usuario"
+                        value={editedUser.username}
+                        onChange={(e) => setEditedUser({ ...editedUser, username: e.target.value })}
+                    />
+                    <br /><br />
+                    <Input
+                        label="Correo electrónico"
+                        value={editedUser.email}
+                        onChange={(e) => setEditedUser({ ...editedUser, email: e.target.value })}
+                    />
+                    <br /><br />
+                    <Label content="Rol" />
+                    <Dropdown
+                        label="Rol"
+                        selection
+                        options={[
+                            { key: 'user', text: 'Usuario', value: 'Usuario' },
+                            { key: 'admin', text: 'Administrador', value: 'Administrador' },
+                        ]}
+                        value={editedUser.rol}
+                        onChange={(e, { value }) => setEditedUser({ ...editedUser, rol: value })}
+                    />
                 </Modal.Content>
+
                 <Modal.Actions>
                     <Button color="blue" onClick={handleSaveChanges}>Guardar Cambios</Button>
                     <Button color="red" onClick={handleCloseModal}>Cancelar</Button>
+                </Modal.Actions>
+            </Modal>
+
+            <Modal open={isAddModalOpen} onClose={handleCloseAddModal}>
+                <Modal.Header>Agregar Usuario</Modal.Header>
+                <Modal.Content>
+                    <Input
+                        label="Nombre de usuario"
+                        value={newUser.Username}
+                        onChange={(e) => setNewUser({ ...newUser, Username: e.target.value })}
+                    />
+                    <br /><br />
+                    <Input
+                        label="Correo electrónico"
+                        value={newUser.Email}
+                        onChange={(e) => setNewUser({ ...newUser, Email: e.target.value })}
+                    />
+                    <br /><br />
+                    <Input
+                        label="Contraseña"
+                        type="password"
+                        value={newUser.Passwrd}
+                        onChange={(e) => setNewUser({ ...newUser, Passwrd: e.target.value })}
+                    />
+                    <br /><br />
+                    <Dropdown
+                        label="Rol"
+                        selection
+                        options={[
+                            { key: 'user', text: 'Usuario', value: 'Usuario' },
+                            { key: 'admin', text: 'Administrador', value: 'Administrador' },
+                        ]}
+                        value={newUser.rol}
+                        onChange={(e, { value }) => setNewUser({ ...newUser, rol: value })}
+                    />
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button color="green" onClick={handleAddUser}>Agregar Usuario</Button>
+                    <Button color="red" onClick={handleCloseAddModal}>Cancelar</Button>
                 </Modal.Actions>
             </Modal>
         </>
