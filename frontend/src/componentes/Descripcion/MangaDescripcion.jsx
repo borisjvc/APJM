0f0462c2e1c95c46f615ebea89d66d278f4730f3
borisjvc <boris_25@live.com.mx>
2023-11-18 13:45:15 -0500

    agregado a lista funcional

--- frontend/src/componentes/Descripcion/MangaDescripcion.jsx
+++ frontend/src/componentes/Descripcion/MangaDescripcion.jsx
@@ -106,7 +106,6 @@ export default function MangaDescripcion({ manga }) {
                     }, 3000);
                 }
             })
-
     };
 
     return (
--- frontend/src/componentes/Descripcion/PeliculasDescripcion.jsx
+++ frontend/src/componentes/Descripcion/PeliculasDescripcion.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
-import { Button, Card, Image } from "semantic-ui-react"
+import { Button, Card, Image, Message, ButtonGroup } from "semantic-ui-react"
 import PlaceholderCard from "../CardPlaceholder";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export default function PeliculasDescripcion({ pelicula }) {
     const [random, setPeliculas] = useState([]);
+    const [message, setMessage] = useState(false);
+    const [messageOk, setMessageOK] = useState(false);
+    const [isAddedToList, setIsAddedToList] = useState(false);
+    const token = localStorage.getItem('token');
     const navigate = useNavigate();
 
     const genresList = pelicula.genres.genres.map(genre => genre.text).join(', ');
@@ -17,7 +22,8 @@ export default function PeliculasDescripcion({ pelicula }) {
 
     useEffect(() => {
         fetchData();
-    }, [])
+        getStatus();
+    }, [pelicula])
 
     const handleCardClick = (Id) => {
         fetchData();
@@ -26,13 +32,114 @@ export default function PeliculasDescripcion({ pelicula }) {
 
     };
 
+    const getStatus = async () => {
+        if (!token) {
+            return
+        }
+
+        const decodedToken = jwtDecode(token);
+        const user = decodedToken.id;
+
+        const Info = { user: user, elemento: pelicula.id, tipo: "Pelicula" };
+
+        axios.post("http://localhost:3001/listas/added", Info)
+            .then(response => {
+                const resultado = response.data.Resultado;  // 'EXISTE' o 'NO_EXISTE'
+                setIsAddedToList(resultado == 'EXISTE');
+            })
+    }
+
+    const addLater = (id) => {
+        if (!token)
+            navigate("/login");
+        else {
+            const decodedToken = jwtDecode(token);
+            const user = decodedToken.id;
+
+            const datos = { user: user, elemento: id, status: "Ver más tarde", tipo: "Pelicula" }
+            axios.post(`http://localhost:3001/listas/agregar`, datos)
+                .then(response => {
+                    if (response.data.affectedRows === 0) {
+                        setMessage(true);
+                        setTimeout(() => {
+                            setMessage(false);
+                        }, 5000);
+                    } else {
+                        setIsAddedToList(true);
+                        setMessageOK(true)
+                        setTimeout(() => {
+                            setMessageOK(false);
+                        }, 3000);
+                    }
+                })
+                .catch(error => {
+                    console.error("Error al agregar a la lista: ", error);
+                });
+        }
+    };
+
+    const removeLater = (id) => {
+        const decodedToken = jwtDecode(token);
+        const user = decodedToken.id;
+        axios.delete(`http://localhost:3001/listas/eliminar/${user}/${id}/Pelicula`)
+            .then(response => {
+                if (response.data.affectedRows > 0) {
+                    setMessageOK(true);
+                    setTimeout(() => {
+                        setMessageOK(false);
+                    }, 3000);
+                } else {
+                    setMessage(true);
+                    setTimeout(() => {
+                        setMessage(false);
+                    }, 3000);
+                }
+            })
+        setIsAddedToList(false);
+    };
+
+    const markAsCompleted = (id) => {
+        const decodedToken = jwtDecode(token);
+        const user = decodedToken.id;
+        const newStatus = { user: user, elemento: id, status: "Completado", tipo: "Pelicula" }
+        axios.put(`http://localhost:3001/listas/status`, newStatus)
+            .then(response => {
+                if (response.data.affectedRows > 0) {
+                    setMessageOK(true);
+                    setTimeout(() => {
+                        setMessageOK(false);
+                    }, 3000);
+                } else {
+                    setMessage(true);
+                    setTimeout(() => {
+                        setMessage(false);
+                    }, 3000);
+                }
+            })
+    };
+
     return (
         <>
             <aside className="left-aside">
                 <img src={pelicula.primaryImage.url} alt={pelicula.titleText.text} className="movie-image" />
                 <br></br>
                 <br></br>
-                <Button content="Ver más tarde" icon='bookmark' labelPosition='left' compact color="blue" />{/* Si ya está en ver más tarde mostrar Eliminar / Marcar como completado */}
+
+                {!isAddedToList ? (
+                    <Button content="Ver más tarde" icon='bookmark' labelPosition='left' compact color="blue" onClick={() => addLater(pelicula.id)} />
+                ) : (
+                    <ButtonGroup>
+                        <Button content="Completado" icon='check circle' labelPosition='left' compact color="blue" onClick={() => markAsCompleted(pelicula.id)} />
+                        <Button content="Eliminar" icon='remove circle' labelPosition='left' compact color="red" onClick={() => removeLater(pelicula.id)} />
+                    </ButtonGroup>
+                )}
+
+                {message && (
+                    <Message content="Ha ocurrido un error" color="red"></Message>
+                )}
+                {messageOk && (
+                    <Message content="Listo" color="teal" icon="check"></Message>
+                )}
 
                 <br></br>
                 <br></br>
