827a62e4dbefd0baf8501cb76121ed78e64a40cd
borisjvc <boris_25@live.com.mx>
2023-11-19 17:40:53 -0500

    login actualizado

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,34 +1,28 @@
-import { Button, Form, Grid, Header, Segment, Message } from 'semantic-ui-react';
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { Button, Form, Grid, Header, Segment, Message, Divider } from 'semantic-ui-react';
+import { useGoogleLogin } from '@react-oauth/google';
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 const LoginForm = () => {
-    const [user, setUser] = useState(null);
+    const [notificationVisible, setNotificationVisible] = useState(false);
     const [profile, setProfile] = useState(null);
+    const navigate = useNavigate();
     const [formValues, setFormValues] = useState({
         correo: '',
         password: '',
     });
 
-    const navigate = useNavigate();
 
-    const [notificationVisible, setNotificationVisible] = useState(false);
+
+
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-        if(token)
+        if (token)
             navigate("/perfil")
     }, []);
-
-    // log out function to log the user out of google and set the profile array to null
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-    };
-
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -36,6 +30,7 @@ const LoginForm = () => {
             ...formValues, [name]: value,
         });
     };
+
 
     const handleLogin = () => {
         const userData = {
@@ -51,7 +46,7 @@ const LoginForm = () => {
                     localStorage.setItem('token', token);
                     const decodedToken = jwtDecode(token);
 
-                    if(decodedToken.rol == "Administrador")
+                    if (decodedToken.rol == "Administrador")
                         navigate("/dashboard/usuarios");
                     else
                         navigate("/");
@@ -62,19 +57,43 @@ const LoginForm = () => {
             })
             .catch(error => {
                 console.error("Error al iniciar sesión: ");
-                
+
             });
     };
 
-    const responseMessage = (response) => {
-        setUser(response);
-        console.log(response);
-    };
-    const errorMessage = (error) => {
-        console.log("error", error);
-    };
+    const login = useGoogleLogin({
+        onSuccess: async (codeResponse) => {
+            await createProfile(codeResponse);
+        },
+        onError: (error) => console.log('Login Failed:', error)
+    });
 
 
+    async function createProfile(user) {
+        if (user) {
+            try {
+                const response = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                    headers: {
+                        Authorization: `Bearer ${user.access_token}`,
+                        Accept: 'application/json'
+                    }
+                });
+                console.log(response.data)
+                const datos = {
+                    Username: response.data.name,
+                    Email: response.data.email,
+                    Passwrd: response.name
+                }
+                const resRegistro = await axios.post("http://localhost:3001/usuarios", datos);
+
+                //const token = response.data.token;
+                //localStorage.setItem('token', token);
+                //navigate("/");
+            } catch (error) {
+                console.log("Error al obtener el perfil del usuario:", error);
+            }
+        }
+    }
     return (
         <Grid textAlign="center" verticalAlign="middle" style={{ height: '100vh' }}>
             <Grid.Column style={{ maxWidth: 450 }}>
@@ -116,40 +135,16 @@ const LoginForm = () => {
                         <Button color="red" href="/">
                             Cancelar
                         </Button>
+
                         <Link to="/registro">
                             <Button color="blue" appearance="subtle">Crear cuenta</Button>
                         </Link>
                         <br />
                         <br />
-
-                        <div>
-                            <br />
-                            <br />
-                            {profile ? (
-                                <div>
-                                    <img src={profile.picture} alt="user image" />
-                                    <h3>User Logged in</h3>
-                                    <p>Name: {profile.name}</p>
-                                    <p>Email Address: {profile.email}</p>
-                                    <br />
-                                    <br />
-                                    <button onClick={logOut}>Log out</button>
-                                </div>
-                            ) : (
-                                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-                            )}
-
-                        </div>
-                        <div>
-                            <br />
-                            <br />
-
-                        </div>
-
+                        <Divider horizontal content="O" />
+                        <Button onClick={login}>Iniciar sesión con Google</Button>
                     </Segment>
-
                 </Form>
-
             </Grid.Column>
         </Grid>
 
